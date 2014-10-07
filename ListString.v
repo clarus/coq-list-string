d4711d0b7b352175d762a34a4f54086677332523
Guillaume Claret <dev@clarus.me>
2014-10-07 14:19:48 +0200

    Nicer proofs for equality functions

--- ListString.v
+++ ListString.v
@@ -38,8 +38,12 @@ Module Bool.
     | true, true | false, false => Eq
     end.
 
-  Lemma compare_sound : forall (x y : bool), compare x y = Eq <-> x = y.
-    intros x y; destruct x; destruct y; simpl; split; congruence.
+  Lemma compare_same_is_eq : forall (x : bool), compare x x = Eq.
+    intro x; destruct x; simpl; reflexivity.
+  Qed.
+
+  Lemma compare_implies_eq : forall (x y : bool), compare x y = Eq -> x = y.
+    intros x y; destruct x; destruct y; simpl; congruence.
   Qed.
 End Bool.
 
@@ -52,37 +56,32 @@ Module Ascii.
       Bool.compare x5 y5 >> Bool.compare x6 y6 >> Bool.compare x7 y7 >> Bool.compare x8 y8
     end.
 
-  Lemma compare_sound : forall (x y : Ascii.ascii), compare x y = Eq <-> x = y.
+  Lemma compare_same_is_eq : forall (x : Ascii.ascii), compare x x = Eq.
+    intro x; destruct x as [x0 x1 x2 x3 x4 x5 x6 x7]; simpl.
+    now repeat (rewrite Bool.compare_same_is_eq).
+  Qed.
+
+  Lemma compare_implies_eq : forall (x y : Ascii.ascii), compare x y = Eq -> x = y.
     intros x y;
       destruct x as [x0 x1 x2 x3 x4 x5 x6 x7];
-      destruct y as [y0 y1 y2 y3 y4 y5 y6 y7]; split; simpl.
-    - intro Hcomp7.
-      destruct (compose_compare_eq _ _ Hcomp7) as [Hcomp6 H7];
-      destruct (compose_compare_eq _ _ Hcomp6) as [Hcomp5 H6];
-      destruct (compose_compare_eq _ _ Hcomp5) as [Hcomp4 H5];
-      destruct (compose_compare_eq _ _ Hcomp4) as [Hcomp3 H4];
-      destruct (compose_compare_eq _ _ Hcomp3) as [Hcomp2 H3];
-      destruct (compose_compare_eq _ _ Hcomp2) as [Hcomp1 H2];
-      destruct (compose_compare_eq _ _ Hcomp1) as [H0 H1].
-      rewrite (proj1 (Bool.compare_sound _ _) H0);
-      rewrite (proj1 (Bool.compare_sound _ _) H1);
-      rewrite (proj1 (Bool.compare_sound _ _) H2);
-      rewrite (proj1 (Bool.compare_sound _ _) H3);
-      rewrite (proj1 (Bool.compare_sound _ _) H4);
-      rewrite (proj1 (Bool.compare_sound _ _) H5);
-      rewrite (proj1 (Bool.compare_sound _ _) H6);
-      rewrite (proj1 (Bool.compare_sound _ _) H7).
-      reflexivity.
-    - intro Heq; injection Heq; intros H7 H6 H5 H4 H3 H2 H1 H0.
-      rewrite (proj2 (Bool.compare_sound _ _) H0);
-      rewrite (proj2 (Bool.compare_sound _ _) H1);
-      rewrite (proj2 (Bool.compare_sound _ _) H2);
-      rewrite (proj2 (Bool.compare_sound _ _) H3);
-      rewrite (proj2 (Bool.compare_sound _ _) H4);
-      rewrite (proj2 (Bool.compare_sound _ _) H5);
-      rewrite (proj2 (Bool.compare_sound _ _) H6);
-      rewrite (proj2 (Bool.compare_sound _ _) H7).
-      reflexivity.
+      destruct y as [y0 y1 y2 y3 y4 y5 y6 y7]; simpl.
+    intro Hcomp7.
+    destruct (compose_compare_eq _ _ Hcomp7) as [Hcomp6 H7];
+    destruct (compose_compare_eq _ _ Hcomp6) as [Hcomp5 H6];
+    destruct (compose_compare_eq _ _ Hcomp5) as [Hcomp4 H5];
+    destruct (compose_compare_eq _ _ Hcomp4) as [Hcomp3 H4];
+    destruct (compose_compare_eq _ _ Hcomp3) as [Hcomp2 H3];
+    destruct (compose_compare_eq _ _ Hcomp2) as [Hcomp1 H2];
+    destruct (compose_compare_eq _ _ Hcomp1) as [H0 H1].
+    rewrite (Bool.compare_implies_eq _ _ H0);
+    rewrite (Bool.compare_implies_eq _ _ H1);
+    rewrite (Bool.compare_implies_eq _ _ H2);
+    rewrite (Bool.compare_implies_eq _ _ H3);
+    rewrite (Bool.compare_implies_eq _ _ H4);
+    rewrite (Bool.compare_implies_eq _ _ H5);
+    rewrite (Bool.compare_implies_eq _ _ H6);
+    rewrite (Bool.compare_implies_eq _ _ H7).
+    reflexivity.
   Qed.
 End Ascii.
 
@@ -94,54 +93,35 @@ Fixpoint compare (x y : t) : comparison :=
   | x :: xs, y :: ys => Ascii.compare x y >> compare xs ys
   end.
 
+Lemma compare_same_is_eq : forall (x : t), compare x x = Eq.
+  intro x; induction x; simpl; trivial.
+  now rewrite Ascii.compare_same_is_eq; rewrite IHx.
+Qed.
+
+Lemma compare_implies_eq : forall (x y : t), compare x y = Eq -> x = y.
+  induction x as [|a x HI]; destruct y as [|b y]; simpl; try congruence.
+  case_eq (Ascii.compare a b); simpl; try congruence.
+  now intros Hab Hxy; rewrite (Ascii.compare_implies_eq _ _ Hab); rewrite (HI _ Hxy).
+Qed.
+
 Definition eqb (x y : t) : bool :=
   match compare x y with
   | Eq => true
   | _ => false
   end.
 
-Lemma string_dec_sound : forall x y, eqb x y = true <-> x = y.
-  induction x; destruct y; unfold eqb; simpl; split; try congruence.
-  - case_eq (compose_compare (Ascii.compare a a0) (compare x y)); intro H; try congruence.
-    destruct (compose_compare_eq _ _ H) as [Hchar Hstring].
-    rewrite (proj1 (Ascii.compare_sound _ _) Hchar).
-    assert (Hstring' : eqb x y = true); [unfold eqb; now rewrite Hstring |].
-    now rewrite (proj1 (IHx _) Hstring').
-  - intro Heq; injection Heq; intros Hstring Hchar.
-    rewrite (proj2 (Ascii.compare_sound _ _) Hchar).
-    assert (H := proj2 (IHx  _) Hstring).
-    unfold eqb in H.
-    congruence.
-    
-    try (unfold eqb; simpl; congruence).
-  unfold eqb; simpl.
-  destruct (Ascii.compare a a0); try congruence.
-congruence.
-  unfold eqb. simpl.
-trivial.
-trivial.
-intuition.
-tauto.
-intuition.
-  intros x y; split.
-  - admit.
-  - intro Heq.
-  
-  induction l; destruct r; simpl; split; try solve [ intuition ; congruence ];
-  consider (ascii_dec a a0); intros; subst. f_equal. eapply IHl; auto.
-  apply IHl. congruence.
-  inversion H. congruence.
+Lemma eqb_same_is_eq : forall (x : t), eqb x x = true.
+  now intros; unfold eqb; rewrite compare_same_is_eq.
 Qed.
 
-(*Global Instance RelDec_string : RelDec (@eq string) :=
-{| rel_dec := string_dec |}.
-
-Global Instance RelDec_Correct_string : RelDec_Correct RelDec_string.
-Proof.
-  constructor; auto using string_dec_sound.
+Lemma eqb_implies_eq : forall (x y : t), eqb x y = true -> x = y.
+  intros x y; unfold eqb; case_eq (compare x y); try congruence.
+  now intros; apply compare_implies_eq.
 Qed.
 
-Global Instance Reflect_string_dec a b : Reflect (string_dec a b) (a = b) (a <> b).
-Proof.
-  apply iff_to_reflect; auto using string_dec_sound.
-Qed.*)
+Definition eq_dec (x y : t) : {x = y} + {x <> y}.
+  case_eq (eqb x y); intro Heqb; [left | right].
+  - now apply eqb_implies_eq.
+  - intro Heq; rewrite Heq in Heqb.
+    rewrite eqb_same_is_eq in Heqb; congruence.
+Qed.
