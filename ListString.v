f744d98fe515cf470d4524652dd9fbe35a0afd35
Guillaume Claret <dev@clarus.me>
2014-10-07 15:59:56 +0200

    Tests added for non-proved functions

--- ListString.v
+++ ListString.v
@@ -56,5 +56,22 @@ Definition split_limit (s : t) (c : ascii) (limit : nat) : list t :=
   split_limit_aux s c [] limit.
 
 Module Tests.
-  (* TODO *)
+  Local Open Scope char.
+
+  Definition test_to_string :
+    to_string ["h"; "e"; "l"; "l"; "o"] = "hello" %string :=
+    eq_refl.
+
+  Definition test_of_string :
+    of_string "hello" = ["h"; "e"; "l"; "l"; "o"] :=
+    eq_refl.
+
+  Definition test_split_1 :
+    split (of_string "go stop go") " " =
+      [of_string "go"; of_string "stop"; of_string "go"] :=
+    eq_refl.
+
+  Definition test_split_2 :
+    split (of_string "grr") " " = [of_string "grr"] :=
+    eq_refl.
 End Tests.
