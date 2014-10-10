979ca85acf7a2ec8aa13b5247fef5bb92ca25acf
Guillaume Claret <dev@clarus.me>
2014-10-10 11:19:05 +0200

    Simplified comparison of characters

--- Char.v
+++ Char.v
@@ -4,53 +4,22 @@ Require "Bool".
 
 Local Open Scope char.
 
-Definition compose_compare (x y : comparison) : comparison :=
-  match x with
-  | Eq => y
-  | _ => x
-  end.
-
-Local Notation "x >> y" := (compose_compare x y) (only parsing, at level 30).
-
-Lemma compose_compare_eq : forall (x y : comparison), compose_compare x y = Eq -> x = Eq /\ y = Eq.
-  intros x y; destruct x; destruct y; simpl; split; congruence.
-Qed.
-
 (** Total order on characters. *)
 Definition compare (x y : Ascii.ascii) : comparison :=
-  match x, y with
-  | Ascii.Ascii x1 x2 x3 x4 x5 x6 x7 x8,
-    Ascii.Ascii y1 y2 y3 y4 y5 y6 y7 y8 =>
-    Bool.compare x1 y1 >> Bool.compare x2 y2 >> Bool.compare x3 y3 >> Bool.compare x4 y4 >>
-    Bool.compare x5 y5 >> Bool.compare x6 y6 >> Bool.compare x7 y7 >> Bool.compare x8 y8
-  end.
+  N.compare (Ascii.N_of_ascii x) (Ascii.N_of_ascii y).
 
-Lemma compare_implies_eq : forall (x y : Ascii.ascii), compare x y = Eq -> x = y.
-  intros x y;
-    destruct x as [x0 x1 x2 x3 x4 x5 x6 x7];
-    destruct y as [y0 y1 y2 y3 y4 y5 y6 y7]; simpl.
-  intro Hcomp7.
-  destruct (compose_compare_eq _ _ Hcomp7) as [Hcomp6 H7];
-  destruct (compose_compare_eq _ _ Hcomp6) as [Hcomp5 H6];
-  destruct (compose_compare_eq _ _ Hcomp5) as [Hcomp4 H5];
-  destruct (compose_compare_eq _ _ Hcomp4) as [Hcomp3 H4];
-  destruct (compose_compare_eq _ _ Hcomp3) as [Hcomp2 H3];
-  destruct (compose_compare_eq _ _ Hcomp2) as [Hcomp1 H2];
-  destruct (compose_compare_eq _ _ Hcomp1) as [H0 H1].
-  rewrite (Bool.compare_implies_eq _ _ H0);
-  rewrite (Bool.compare_implies_eq _ _ H1);
-  rewrite (Bool.compare_implies_eq _ _ H2);
-  rewrite (Bool.compare_implies_eq _ _ H3);
-  rewrite (Bool.compare_implies_eq _ _ H4);
-  rewrite (Bool.compare_implies_eq _ _ H5);
-  rewrite (Bool.compare_implies_eq _ _ H6);
-  rewrite (Bool.compare_implies_eq _ _ H7).
-  reflexivity.
+Lemma compare_implies_eq : forall (x y : Ascii.ascii),
+  compare x y = Eq -> x = y.
+  unfold compare; intros x y H.
+  rewrite <- ascii_N_embedding with (a := x);
+    rewrite <- ascii_N_embedding with (a := y).
+  f_equal.
+  now apply N.compare_eq_iff.
 Qed.
 
 Lemma compare_same_is_eq : forall (x : Ascii.ascii), compare x x = Eq.
-  intro x; destruct x as [x0 x1 x2 x3 x4 x5 x6 x7]; simpl.
-  now repeat (rewrite Bool.compare_same_is_eq).
+  intro x; unfold compare.
+  now apply N.compare_eq_iff.
 Qed.
 
 (** Test if two characters are equal. *)
