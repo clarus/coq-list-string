5cdb9183ff8baa47a31a52a6441052d5103febbb
Guillaume Claret <dev@clarus.me>
2014-10-10 10:23:11 +0200

    ListString replaced by LString

---
+++ LString.v
@@ -0,0 +1,14 @@
+Require "Comparison".
+Require "Conversion".
+Require "Definition".
+Require "Etc".
+
+Include LString.Comparison.
+Include LString.Conversion.
+Include LString.Definition.
+Include LString.Etc.
+
+Module Char.
+  Require "Char".
+  Include LString.Char.
+End Char.
