d0089d933d79d2a2efdc440865be0a2d5d5f05a1
Guillaume Claret <dev@clarus.me>
2014-10-10 14:08:47 +0200

    Missing includes added

--- LString.v
+++ LString.v
@@ -1,12 +1,16 @@
+Require "Case".
 Require "Comparison".
 Require "Conversion".
 Require "Definition".
 Require "Etc".
+Require "Trim".
 
+Include LString.Case.
 Include LString.Comparison.
 Include LString.Conversion.
 Include LString.Definition.
 Include LString.Etc.
+Include LString.Trim.
 
 Module Char.
   Require "Char".
