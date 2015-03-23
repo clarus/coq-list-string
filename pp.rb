0e607385e537a9146c80f58b9bda85329e7f8062
Guillaume Claret <dev@clarus.me>
2015-03-23 17:14:00 +0100

    Sources moved to src/

--- pp.rb
+++ pp.rb
@@ -4,7 +4,7 @@ require 'erb'
 # The version of Coq.
 version = `coqc -v`.match(/version ([^(]*) \(/)[1]
 
-Dir.glob("*.v.erb") do |file_name|
+Dir.glob("src/*.v.erb") do |file_name|
   renderer = ERB.new(File.read(file_name, encoding: "UTF-8"))
   File.open(file_name[0..-5], "w") do |file|
     file << renderer.result()
